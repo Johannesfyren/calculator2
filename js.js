2e25cfddd95086bce27ad807f4aec4c2206e5c7b
JohannesFyren <hergaardjohannes@gmail.com>
2024-03-29 14:15:37 +0100

    Add basic files and HTML structure

---
+++ js.js
@@ -0,0 +1,22 @@
+let firstNumber, secondNumber, operator;
+
+
+function operate(firstNumber, secondNumber, operator){
+    add (firstNumber,secondNumber);
+}
+
+
+
+
+function add (a,b){
+    return a+b;
+}
+function subtract (a,b){
+    return a-b;
+}
+function multiply (a,b){
+    return a*b;
+}
+function divide (a,b){
+    return a/b;
+}
